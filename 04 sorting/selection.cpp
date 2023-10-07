9ce1775542246ea17f13c9d95638c94041f761e3
Shiva Yadav <shivaydv1256@gmail.com>
2023-10-07 21:02:38 +0530

    email changed

--- 04 sorting/selection.cpp
+++ 04 sorting/selection.cpp
@@ -2,16 +2,13 @@
 using namespace std;
 void sort(int arr[], int len)
 {
-    for (int i = 0; i < len - 1; i++)
-    {
+    for (int i = 0; i < len - 1; i++){
         int min = i;
-        for (int j = i; j < len; j++)
-        {
+        for (int j = i; j < len; j++){
             if (arr[j] < arr[min])
                 min = j;
         }
-        if (i != min)
-        {
+        if (i != min){
             int temp = arr[i];
             arr[i] = arr[min];
             arr[min] = temp;
@@ -23,8 +20,7 @@ int main()
     int arr[] = {1, 5, 4, 56, 86, 12, 36, 45};
     int len = sizeof(arr) / sizeof(arr[0]);
     cout<<"Before Sorting"<<endl;
-    for (int i = 0; i < len; i++)
-    {
+    for (int i = 0; i < len; i++){
         cout << arr[i] << " ";
     }
     cout << endl;
@@ -32,8 +28,7 @@ int main()
     sort(arr, len);
 
     cout<<"After Sorting \n";
-    for (int i = 0; i < len; i++)
-    {
+    for (int i = 0; i < len; i++){
         cout << arr[i] << " ";
     }
     cout << endl;
